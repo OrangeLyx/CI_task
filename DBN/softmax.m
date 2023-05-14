21b2e771792278d37355d6bf369ac5840cca5a67
eta <98522124+OrangeLyx@users.noreply.github.com>
2023-05-14 17:21:58 +0800

    Add files via upload

---
+++ DBN/softmax.m
@@ -0,0 +1,10 @@
+function a = softmax(n)
+
+nmax = max(n,[],2);
+n = bsxfun(@minus,n,nmax);
+
+numer = exp(n);
+denom = sum(numer,2); 
+denom(denom == 0) = 1;
+a = bsxfun(@rdivide,numer,denom);
+end
